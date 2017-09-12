3c7531372d5d04c7f41bddb01c0f1e80eb964c48
Mahfoud DROUAZ <drouaz_mahfoud@yahoo.fr>
2017-09-12 12:55:32 +0200

    Ceci est la première version du code CalculPuissance, le fichier actuel contient l'entête du code

---
+++ CalculPuissance.m
@@ -0,0 +1,6 @@
+% Ceci est le second fichier du projet, il contient l'entete d'un code matlab
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Mahfoud DROUAZ
+%%% Code matlab traitement des signaux électriques
+%%% Ce code réalise la calcul de la puissance instantanée absorbée par les charges électriques
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

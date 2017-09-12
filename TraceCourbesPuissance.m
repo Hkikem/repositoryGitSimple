c49dc5c58d3f9c733a4702479bd494c548dc47ad
Mahfoud DROUAZ <drouaz_mahfoud@yahoo.fr>
2017-09-12 12:58:07 +0200

    Ceci est la deuxième version de code matlab TraceCourbesPuissance.m, le path et le chargement du fichier des données ont été rajoutés, le code permet de tracer une première courbe

--- TraceCourbesPuissance.m
+++ TraceCourbesPuissance.m
@@ -3,3 +3,13 @@
 %%% Mahfoud DROUAZ
 %%% Code matlab qui trace les courbes des puissances
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc, clear all, close all
+
+addpath"/home/mahfoud/Bureau/repositoryGitSimple";
+
+load"nom_fichier_des_mesures.mat";
+
+figure(1);
+plot(time,P);
+xlabel("Time(s)");
+ylabel("P(Watt");
